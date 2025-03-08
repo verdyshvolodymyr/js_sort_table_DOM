b2c99fe9c454fd3f64607326ddc821cf9cc352c4
Verdysh Volodymyr <verdyshvladimir@gmail.com>
2025-03-08 12:42:45 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+document.addEventListener('click', sortTable);
+
+function sortTable(e) {
+  const table = document.querySelector('table');
+  const tbody = table.querySelector('tbody');
+  const rows = Array.from(tbody.querySelectorAll('tr'));
+  let index = 0;
+
+  if (e.target.textContent === 'Position') {
+    index = 1;
+  }
+
+  if (e.target.textContent === 'Age') {
+    index = 2;
+  }
+
+  if (e.target.textContent === 'Salary') {
+    index = 3;
+  }
+
+  rows.sort((rowA, rowB) => {
+    const cellA = rowA.cells[index].textContent.trim();
+    const cellB = rowB.cells[index].textContent.trim();
+
+    return isNaN(cellA - cellB) ? cellA.localeCompare(cellB) : cellA - cellB;
+  });
+
+  rows.forEach((row) => tbody.appendChild(row));
+}
